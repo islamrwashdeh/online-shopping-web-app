2b54e029666afdccfbf3a9f845af90940babeef4
islam rwashdeh <57620910+islamrwashdeh@users.noreply.github.com>
2022-10-27 11:14:34 +0000

    backend 01

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const cookieParser = require('cookie-parser')
+const path = require('path')
+
 
 
 const app = express()
@@ -17,6 +19,10 @@ app.use(fileUpload({
 
 // Routes
 app.use('/user', require('./routes/userRouter'))
+app.use('/api', require('./routes/categoryRouter'))
+app.use('/api', require('./routes/upload'))
+app.use('/api', require('./routes/productRouter'))
+app.use('/api', require('./routes/paymentRouter'))
 
 
 
@@ -30,13 +36,20 @@ mongoose.connect(URI,
         console.log('connected to MongoDB')
     });
 
+    if(process.env.NODE_ENV === 'production'){
+        app.use(express.static('client/build'))
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        })
+    }
+    
 
-
-app.get('/',(req, res)=>{
-    res.json({msg: "hiiiiiiiiiiiiiiiiiiiiiiiiiiii"})
-})
+// app.get('/',(req, res)=>{
+//     res.json({msg: "hiiiiiiiiiiiiiiiiiiiiiiiiiiii"})
+// })
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () =>{
     console.log('Server is running on port', PORT)
+
 })
